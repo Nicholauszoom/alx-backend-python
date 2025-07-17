d88a08b2efcdebc0e257b18ba1861f581e1436db
nicholauszoom <nicholaussomi5@gmail.com>
2025-07-17 23:49:39 +0300

    add nested map exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,10 +6,19 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase) :
     """unit test for access_nested_map"""
 
+    # @parameterized.expand([
+    #      ({"a": 1}, ("a",), 1),
+    # ({"a": {"b": 2}}, ("a",), {"b": 2}),
+    # ({"a": {"b": 2}}, ("a", "b"), 2),
+    # ])
+    # def test_access_nested_map(self, nested_map, path, expected):
+    #     self.assertEqual(access_nested_map(nested_map, path),expected)
+
     @parameterized.expand([
-         ({"a": 1}, ("a",), 1),
-    ({"a": {"b": 2}}, ("a",), {"b": 2}),
-    ({"a": {"b": 2}}, ("a", "b"), 2),
+        nested_map={}, path=("a",)
+        nested_map={"a": 1}, path=("a", "b")
     ])
-    def test_access_nested_map(self, nested_mapp, path, expected):
-        self.assertEqual(access_nested_map(nested_mapp, path),expected)
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -2,5 +2,8 @@
 def access_nested_map(nested_map, path):
     """Access a value in a nested map using a sequence of keys."""
     for key in path:
+        if not isinstance(nested_map, dict):
+            raise KeyError(key)
         nested_map = nested_map[key]
     return nested_map
+
