7c3ec0395357c99bf2f9475ceac0d9fcd1d659c6
nicholauszoom <nicholaussomi5@gmail.com>
2025-07-20 21:51:06 +0300

    add small change

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
