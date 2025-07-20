c3855c1512f8c7ef5cf3770036c319338c9c93c7
nicholauszoom <nicholaussomi5@gmail.com>
2025-07-20 22:28:12 +0300

    add small change

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
      path('api/', include('messaging_app.chats.urls')),
      path('api/', include('chats.urls')),
+     path('api-auth/', include('rest_framework.urls')), 
 ]
