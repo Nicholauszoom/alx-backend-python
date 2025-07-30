9a8b0de733c13ee8a03909908eaed6ea27ed9dcf
nicholauszoom <nicholaussomi5@gmail.com>
2025-07-30 16:49:50 +0300

    message app

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('api/', include('messaging_app.chats.urls')),
-     path('api/', include('chats.urls')),
-     path('api-auth/', include('rest_framework.urls')), 
+    path('api/', include('messaging_app.chats.urls')),
+    path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls')), 
 ]
