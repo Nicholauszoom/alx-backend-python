bc8bb73118f7313aac63b735493740b93c41db73
nicholauszoom <nicholaussomi5@gmail.com>
2025-07-30 16:56:04 +0300

    small change

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
         'rest_framework.authentication.BasicAuthentication',
     ],
 }
